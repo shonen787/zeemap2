aa47dbcf2e99536bd4c39cb643cc16b45968544a
shonen787 <shonen787@gmail.com>
2025-06-04 23:56:41 -0700

    Adding functionality like response  parsing

--- src/imap.zig
+++ src/imap.zig
@@ -17,7 +17,7 @@ pub const ResponseStatus = enum {
     no,
     bad,
     continuation,
-    untragged,
+    untagged,
 };
 
 pub const EmailMessage = struct {
@@ -98,6 +98,26 @@ pub const ImapConnection = struct {
     fn read(self: *Self, buffer: []u8) !usize {
         return self.tls_client.read(self.tcp_socket, buffer);
     }
+
+    fn readLine(self: *Self, buffer: []u8) ![]u8 {
+        var pos: usize = 0;
+        while (pos < buffer.len - 1) {
+            const bytes_read = try self.read(buffer[pos .. pos + 1]);
+            if (bytes_read == 0) break;
+            if (buffer[pos] == '\n') {
+                return buffer[0 .. pos + 1];
+            }
+            pos += 1;
+        }
+        return buffer[0..pos];
+    }
+
+    pub fn readResponse(self: *Self, buffer: []u8) ![]u8 {
+        @memset(buffer, 0);
+        const bytes_read = try self.read(buffer);
+        return buffer[0..bytes_read];
+    }
+
     fn write(self: *Self, data: []const u8) !usize {
         return try self.tls_client.write(self.tcp_socket, data);
     }
@@ -114,5 +134,80 @@ pub const ImapConnection = struct {
 
     fn incrementTag(self: *Self) void {
         self.request_id += 1;
+    }
+
+    pub fn selectMailbox(self: *Self, mailbox: []const u8) !void {
+        log.info("Selecting Mailbox: {s}", .{mailbox});
+        const select_cmd = try std.fmt.allocPrint(self.allocator, "SELECT {s}", .{mailbox});
+        defer self.allocator.free(select_cmd);
+
+        const response = try self.executeCommand(select_cmd);
+        if (response.status != .ok) {
+            log.err("Failed to select mailbox {s}", .{mailbox});
+            return ImapError.CommandFailed;
+        }
+
+        log.info("Successfully selected mailbox: {s}", .{mailbox});
+    }
+
+    fn parseResponse(response: []const u8) ImapResposne {
+        if (std.mem.startsWith(u8, response, "+")) {
+            return ImapResposne{ .tag = "", .status = .continuation, .message = response[1..], .raw = response };
+        }
+
+        var iter = std.mem.splitAny(u8, std.mem.trim(u8, response, " \r\n"), " ");
+        const tag = iter.next() orelse "";
+        const status_str = iter.next() orelse "";
+        const message = iter.rest();
+        var status = ResponseStatus.untagged;
+
+        if (std.mem.eql(u8, status_str, "OK")) status = ResponseStatus.ok;
+        if (std.mem.eql(u8, status_str, "NO")) status = ResponseStatus.no;
+        if (std.mem.eql(u8, status_str, "BAD")) status = ResponseStatus.bad;
+
+        return ImapResposne{
+            .tag = tag,
+            .status = status,
+            .message = message,
+            .raw = response,
+        };
+    }
+
+    pub fn executeCommand(self: *Self, command: []const u8) !ImapResposne {
+        var buffer: [BUFFER_SIZE]u8 = undefined;
+        const tag = try self.getTag();
+        defer self.allocator.free(tag);
+
+        _ = try self.write(tag);
+        _ = try self.write(" ");
+        _ = try self.write(command);
+        _ = try self.write("\r\n");
+
+        log.info("Client: {s}-{s}", .{ tag, command });
+        self.incrementTag();
+
+        if (std.mem.startsWith(u8, command, "SEARCH")) {
+            log.info("executeCommand detected SEARCH command, returning immediately", .{});
+            return ImapResposne{
+                .tag = tag,
+                .status = .ok,
+                .message = "SEACH initiated",
+                .raw = "",
+            };
+        }
+
+        while (true) {
+            const raw_response = try self.readLine(&buffer);
+            log.info("Server: {s}", .{raw_response});
+            const response = parseResponse(raw_response);
+
+            if (std.mem.eql(u8, response.tag, tag)) {
+                return response;
+            }
+
+            if (response.status == .untagged) {
+                continue;
+            }
+        }
     }
 };
